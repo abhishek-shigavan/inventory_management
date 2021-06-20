1547a2a7271a32918be6f3c291587299e7ea95ca
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-20 22:46:43 +0530

    Add: Interface & Method Definations

---
+++ Service/InventoryService.java
@@ -0,0 +1,44 @@
+package inventory.service;
+
+import inventory.dao.InventoryDAO;
+import inventory.model.Inventory;
+/**
+ * InventoryService -- This class contains defination
+ * 					   of methods
+ * 
+ * 
+ * @author Abhishek Shigavan
+ *
+ */
+public class InventoryService implements InventoryServiceInterface {
+
+	InventoryDAO inventoryDAO = new InventoryDAO();
+	
+	public void addInventory(Inventory inventory) {
+		
+		inventoryDAO.addInventory(inventory);	
+	}
+
+	@Override
+	public boolean checkAvailability(String name) {
+		
+		return inventoryDAO.checkAvailability(name);
+	}
+	
+	@Override
+	public void editProduct(String name, Inventory inventory) {
+		
+		inventoryDAO.editProduct(name, inventory);
+	}
+
+	@Override
+	public void deleteProduct(String name) {
+		
+		inventoryDAO.deleteProduct(name);
+	}
+	
+	public void displayInventory() {
+		
+		inventoryDAO.displayInventory();
+	}
+}
